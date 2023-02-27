43cd8d1684cb385e1eddbf9b202281a2583bef90
Steven Reina <nelson.reina132@gmail.com>
2023-02-27 17:34:49 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+// import App from './App'
+import './index.css'
+import { PokemonApp } from './PokemonApp'
+
+import { store } from './store'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <PokemonApp />
+    </Provider>
+  </React.StrictMode>,
+)
